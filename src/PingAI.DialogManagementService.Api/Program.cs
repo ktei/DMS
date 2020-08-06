1e7bdfaec8bbd26bd1eee27c6b1caacff9807ccf
rui <ktei2008@gmail.com>
2020-08-07 02:18:02 +1000

    clean up Program class

--- src/PingAI.DialogManagementService.Api/Program.cs
+++ src/PingAI.DialogManagementService.Api/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -10,14 +7,14 @@ using Microsoft.Extensions.Logging;
 
 namespace PingAI.DialogManagementService.Api
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((context, builder) =>
                 {
@@ -52,6 +49,7 @@ namespace PingAI.DialogManagementService.Api
                         // so we need to listen on https in production
                         options.Listen(IPAddress.Any, portNumber);
                         Console.WriteLine($"Listening on http://*:{portNumber}");
+                        Console.Title = $"DialogManagementService: {portNumber}";
                     });
                     webBuilder.UseStartup<Startup>();
                 });
