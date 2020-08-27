5db2b85c6169546f69f171ae11fe5e546f7c4dea
rui <ktei2008@gmail.com>
2020-08-27 21:36:48 +1000

    authorize integration api

--- src/PingAI.DialogManagementService.Api/Controllers/Integration/IntegrationControllerBase.cs
+++ src/PingAI.DialogManagementService.Api/Controllers/Integration/IntegrationControllerBase.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PingAI.DialogManagementService.Api.Controllers.Integration
 {
+    [Authorize]
     [Route("dms/api/integration/v{version:apiVersion}/[controller]")]
     public abstract class IntegrationControllerBase : ApiControllerBase
     {
