3707472d7dd0863548f196ef7b3927ca001dbcca
rui <ktei2008@gmail.com>
2020-09-09 15:02:32 +1000

    no auth for admin controllers temporarily, because we ran out of m2m tokens this month

--- src/PingAI.DialogManagementService.Api/Controllers/Admin/AdminControllerBase.cs
+++ src/PingAI.DialogManagementService.Api/Controllers/Admin/AdminControllerBase.cs
@@ -4,7 +4,8 @@ using PingAI.DialogManagementService.Api.Authorization;
 
 namespace PingAI.DialogManagementService.Api.Controllers.Admin
 {
-    [Authorize(Policy = Policies.AdminOnly)]
+    // TODO: after our Auth0 quota is back next month, we uncomment this...lol
+    // [Authorize(Policy = Policies.AdminOnly)]
     [Route("dms/api/admin/v{version:apiVersion}/[controller]")]
     public abstract class AdminControllerBase : ApiControllerBase
     {
