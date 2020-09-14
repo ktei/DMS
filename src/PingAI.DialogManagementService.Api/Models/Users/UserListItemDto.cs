988e56d11501919c0a2f9532910e0e1cfd4a79ba
rui <ktei2008@gmail.com>
2020-09-14 17:55:40 +1000

    UserListItemDto

--- src/PingAI.DialogManagementService.Api/Models/Users/UserListItemDto.cs
+++ src/PingAI.DialogManagementService.Api/Models/Users/UserListItemDto.cs
@@ -8,14 +8,14 @@ namespace PingAI.DialogManagementService.Api.Models.Users
         public string UserId { get; set; }
         public string Name { get; set; }
         public string Auth0Id { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public string CreatedAt { get; set; }
 
         public UserListItemDto(User user)
         {
             UserId = user.Id.ToString();
             Name = user.Name;
             Auth0Id = user.Auth0Id;
-            CreatedAt = user.CreatedAt;
+            CreatedAt = user.CreatedAt.ToString("o");
         }
 
         public UserListItemDto()
