acb6670483290d465a7377cd4a06088009f066ea
rui <ktei2008@gmail.com>
2020-10-05 17:34:53 +1100

    update QueryListItemResponse DTO

--- src/PingAI.DialogManagementService.Api/Models/Queries/QueryListItemResponse.cs
+++ src/PingAI.DialogManagementService.Api/Models/Queries/QueryListItemResponse.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Text;
+using PingAI.DialogManagementService.Api.Models.Responses;
 using PingAI.DialogManagementService.Domain.Model;
 
 namespace PingAI.DialogManagementService.Api.Models.Queries
@@ -8,12 +10,17 @@ namespace PingAI.DialogManagementService.Api.Models.Queries
         public string ResponseId { get; set; }
         public string? Text { get; set; }
         public string Type { get; set; }
+        public ResolutionPartDto[]? Parts { get; set; }
+        public FormResolutionDto? Form { get; set; }
 
-        public QueryListItemResponse(string responseId, string type, string? text)
+        public QueryListItemResponse(string responseId, string type, string? text,
+            ResolutionPartDto[]? parts, FormResolutionDto? form)
         {
             ResponseId = responseId;
             Type = type;
             Text = text;
+            Parts = parts;
+            Form = form;
         }
 
         public QueryListItemResponse(Response response)
@@ -21,6 +28,14 @@ namespace PingAI.DialogManagementService.Api.Models.Queries
             ResponseId = response.Id.ToString();
             Type = response.Type.ToString();
             Text = GetResponseText(response);
+            if (response.Resolution?.Type == ResolutionType.PARTS)
+            {
+                Parts = response.Resolution.Parts?.Select(p => new ResolutionPartDto(p)).ToArray();
+            }
+            else if (response.Resolution?.Type == ResolutionType.FORM)
+            {
+                Form = response.Resolution.Form == null ? default : new FormResolutionDto(response.Resolution.Form);
+            }
         }
 
         public QueryListItemResponse()
