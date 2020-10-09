58a77a5cea785cc4b7a860a06227225fedaee080
rui <ktei2008@gmail.com>
2020-10-09 19:54:19 +1100

    bring admin auth back

--- src/PingAI.DialogManagementService.Api/Controllers/Admin/AdminControllerBase.cs
+++ src/PingAI.DialogManagementService.Api/Controllers/Admin/AdminControllerBase.cs
@@ -4,8 +4,7 @@ using PingAI.DialogManagementService.Api.Authorization;
 
 namespace PingAI.DialogManagementService.Api.Controllers.Admin
 {
-    // TODO: after our Auth0 quota is back next month, we uncomment this...lol
-    // [Authorize(Policy = Policies.AdminOnly)]
+    [Authorize(Policy = Policies.AdminOnly)]
     [Route("dms/api/admin/v{version:apiVersion}/[controller]")]
     public abstract class AdminControllerBase : ApiControllerBase
     {
