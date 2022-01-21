90f9d64c41b7a982f7e715ed0930e3ed8601cb4d
Steve Mao <maochenyan@gmail.com>
2022-01-21 18:39:39 +1100

    fix response entity, same as https://github.com/pingai-github/DialogManagementService/pull/25/files

--- src/PingAI.DialogManagementService.Domain/Model/ResolutionPart.cs
+++ src/PingAI.DialogManagementService.Domain/Model/ResolutionPart.cs
@@ -6,6 +6,9 @@ namespace PingAI.DialogManagementService.Domain.Model
 {
     public class ResolutionPart : ValueObject
     {
+        [JsonPropertyName("path")]
+        public string? Path { get; set; }
+        
         [JsonPropertyName("text")]
         public string? Text { get; set; }
         
@@ -17,17 +20,18 @@ namespace PingAI.DialogManagementService.Domain.Model
         public ResolutionPartType Type { get; set; }
 
         [JsonConstructor]
-        public ResolutionPart(string? text, Guid? entityNameId, ResolutionPartType type)
+        public ResolutionPart(string? text, Guid? entityNameId, ResolutionPartType type, string? path)
         {
             Text = text;
             EntityNameId = entityNameId;
             Type = type;
+            Path = path;
         }
 
         public static ResolutionPart TextPart(string text) => 
-            new ResolutionPart(text ?? throw new ArgumentNullException(nameof(text)), null, ResolutionPartType.RTE);
-        public static ResolutionPart EntityNamePart(Guid entityNameId) =>
-            new ResolutionPart(null, entityNameId, ResolutionPartType.OTHER_ENTITY_NAME);
+            new ResolutionPart(text ?? throw new ArgumentNullException(nameof(text)), null, ResolutionPartType.RTE, null);
+        public static ResolutionPart EntityNamePart(Guid entityNameId, string? path) =>
+            new ResolutionPart(null, entityNameId, ResolutionPartType.OTHER_ENTITY_NAME, path);
 
         protected override IEnumerable<object> GetAtomicValues()
         {
